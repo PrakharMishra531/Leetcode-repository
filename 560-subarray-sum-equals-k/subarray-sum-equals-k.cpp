cdcbf91a075b79f909561bb66fc2985c44815d29
Prakhar Mishra <142316576+PrakharMishra531@users.noreply.github.com>
2023-10-27 23:45:04 +0530

    Time: 115 ms (10.00%) | Memory: 50.3 MB (7.64%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+    int n = nums.size();
+    map<int,int> mpp;
+    int preSum = 0, cnt = 0;
+
+    mpp[0] = 1; 
+    for (int i = 0; i < n; i++) {
+        preSum += nums[i];
+        int remove = preSum - k;
+        cnt += mpp[remove];
+        mpp[preSum] += 1;
+    }
+    return cnt;
+    }
+};
