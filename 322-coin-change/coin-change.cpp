d75525669a86d61000e8015251fb7882810fc547
Voyager <142316576+PrakharMishra531@users.noreply.github.com>
2024-09-03 15:37:47 +0530

    Time: 26 ms (72.80%) | Memory: 13 MB (93.41%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int dp[10010];
+    int func(int amount, vector<int>& coins){
+        if(amount == 0)return 0;
+        if(dp[amount]!=-1)return dp[amount];
+        int ans = INT_MAX;
+        for(auto coin : coins){
+            if(amount-coin>=0)
+            ans = min(ans + 0ll, func(amount-coin,coins)+1ll);
+        }
+        return dp[amount]=ans;
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        memset(dp,-1,sizeof(dp));
+        int ans = func(amount,coins);
+        return ans==INT_MAX?-1:ans;
+    }
+};
